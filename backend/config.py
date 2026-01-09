b15f55b3462b808c34700a6e446d190aec73100d
kaanmete <kaanmetekucuk286.kmkk@gmail.com>
2026-01-09 16:06:46 +0300

    Update config.py
    sql file name corrected

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'safe-and-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or ''
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql:///schema.sql'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
