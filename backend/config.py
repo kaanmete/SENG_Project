5473ffb3e6bae1ac2fe368febc60461f9da12791
kaanmete <kaanmetekucuk286.kmkk@gmail.com>
2026-01-14 17:21:54 +0300

    restart
    restarting from scratch

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'safe-and-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql:///schema.sql'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
--- backend/controllers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .admin_controller import admin_bp
-from .auth_controller import auth_bp
-from .exam_controller import exam_bp
-from .hint_controller import hint_bp
-from .learning_controller import learning_bp
-from .result_controller import result_bp
